f8252aff24394163f3d5e45654cd1da426c54d8f
huatrung <vinhtrung1412@gmail.com>
2022-06-08 22:31:41 +0700

    backend_cources

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 1994;
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//[POST] middleware
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//http log
+app.use(morgan('combined'));
+
+//template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: 'hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+//routes init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
