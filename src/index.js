0ef5937b6c6107d023623816a1b3b069e59fffd7
huatrung <vinhtrung1412@gmail.com>
2022-06-09 15:58:23 +0700

    card-bs4

--- src/index.js
+++ src/index.js
@@ -37,5 +37,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
