4e8e2535d1c03fa7e115b2b4b229ef19be0464ae
huatrung <vinhtrung1412@gmail.com>
2022-06-09 20:18:45 +0700

    updated

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,15 @@
 const Course = require('../models/Course');
-const {mutipleMongooseToObject} = require('../../util/mongoose');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     index(req, res, next) {
-      Course.find({})
-        .then(courses => {
-          res.render('home', { 
-            courses: mutipleMongooseToObject(courses)
-          });
-        })
-        .catch(next);
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     search(req, res) {
