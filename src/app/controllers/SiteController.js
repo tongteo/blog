5f6eb35a1ad33e6e820c1483c0bbee4bea45fb22
huatrung <vinhtrung1412@gmail.com>
2022-06-09 13:43:33 +0700

    updated

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,14 @@
 const Course = require('../models/Course');
 
-
 class SiteController {
     index(req, res) {
-
-      Course.find({}, function(err, courses) {
-        if(!err) {
-            res.json(courses);
-            return;
-        }
-        res.status(400).json({error: 'ERROR!!!!'});
-      });
+        Course.find({}, function (err, courses) {
+            if (!err) {
+                res.json(courses);
+                return;
+            }
+            res.status(400).json({ error: 'ERROR!!!!' });
+        });
         //res.render('home');
     }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const Course = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 600 },
@@ -11,4 +10,3 @@ const Course = new Schema({
 });
 
 module.exports = mongoose.model('Course', Course);
-
